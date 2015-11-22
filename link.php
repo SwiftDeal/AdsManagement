2d079a359c07ee9fade2406d7cdf2bf93224af6c
Faizan Ayubi <indianayubi@gmail.com>
2015-11-23 00:20:27 +0530

    Google Analytics Tracking

--- link.php
+++ link.php
@@ -24,12 +24,13 @@ foreach ($datas as $data) {
     <meta property="og:type" content="article">
     <meta property="og:description" content="<?php echo $item['description']?>">
     <meta property="og:image" content="http://www.chocoghar.com/public/assets/uploads/images/<?php echo $item['image'];?>">
-    <meta name="og:image-new" content="http://www.chocoghar.com/public/assets/uploads/images/<?php echo $item['image'];?>">
+    <meta name="og:image-new" content="http://www.chocoghar.com/public/assets/uploads/images/resize/<?php echo $item['image'];?>">
     <meta property="og:site_name" content="The EarnBugs Media Group">
     <meta name="generator" content="SwiftMVC 1.1.1">
 </head>
 
 <body>
+<img src="https://www.google-analytics.com/collect?v=1&tid=UA-70464246-1&cid=".$item["username"]."&t=pageview&dp=".$item["id"]."&ua=ChocoGhar" alt="tracking" />
 <script type="text/javascript">
     window.location.href = '<?php echo $item["url"];?>?utm_source=chocoghar.com&utm_medium=<?php echo $item["username"];?>&utm_campaign=<?php echo $item["title"];?>';
 </script>
