3983b8af0a78f5f56372e6923386f2e3f0009d8d
Faizan Ayubi <indianayubi@gmail.com>
2015-12-12 04:00:36 +0530

    PHP CRON Management

--- application/controllers/cron.php
+++ application/controllers/cron.php
@@ -8,14 +8,126 @@
 
 class CRON extends Auth {
 
+    private $connection;
+    private $path;
+    private $handle;
+    private $cron_file;
+
     public function __construct($options = array()) {
         parent::__construct($options);
-        $this->willRenderLayoutView = false;
-        $this->willRenderActionView = false;
+        $this->noview();
     }
 
+    protected function initialize($host=NULL, $port=NULL, $username=NULL, $password=NULL) {
+        $this->path      = APP_PATH . "/logs/";
+        $this->handle    = 'crontab.txt';  
+        $this->cron_file = "{$this->path}{$this->handle}";
+     
+        try {
+            if (is_null($host) || is_null($port) || is_null($username) || is_null($password)) {
+                throw new Exception("Please specify the host, port, username and password!");
+            }
+             
+            $this->connection = @ssh2_connect($host, $port);
+            if (!$this->connection) {
+                throw new Exception("The SSH2 connection could not be established.");
+            }
+     
+            $authentication = @ssh2_auth_password($this->connection, $username, $password);
+            if (!$authentication) {
+                throw new Exception("Could not authenticate '{$username}' using password: '{$password}'.");
+            }
+        } catch (Exception $e) {
+            $this->error_message($e->getMessage());
+        }
+    }
+
+    protected function exec() {
+        $argument_count = func_num_args();
+        try {
+            if (!$argument_count) {
+                throw new Exception("There is nothing to execute, no arguments specified.");
+            }
+            $arguments = func_get_args();
+            $command_string = ($argument_count > 1) ? implode(" && ", $arguments) : $arguments[0];
+            $stream = @ssh2_exec($this->connection, $command_string);
+            if (!$stream) {
+                throw new Exception("Unable to execute the specified commands: <br />{$command_string}");
+            }
+        } catch (Exception $e) {
+            $this->error_message($e->getMessage());
+        }
+        return $this;
+    }
+
+    protected function write_to_file($path=NULL, $handle=NULL) {
+        if (!$this->crontab_file_exists()) {   
+            $this->handle = (is_null($handle)) ? $this->handle : $handle;
+            $this->path   = (is_null($path))   ? $this->path   : $path;
+     
+            $this->cron_file = "{$this->path}{$this->handle}";
+            $init_cron = "crontab -l > {$this->cron_file} && [ -f {$this->cron_file} ] || > {$this->cron_file}";
+            $this->exec($init_cron);
+        }
+     
+        return $this;
+    }
+
+    protected function remove_file() {
+        if ($this->crontab_file_exists()) {
+            $this->exec("rm {$this->cron_file}");
+        }
+        return $this;
+    }
+
+    protected function append_cronjob($cron_jobs=NULL) {
+        if (is_null($cron_jobs)) {
+            $this->error_message("Nothing to append!  Please specify a cron job or an array of cron jobs.");
+        }
+        
+        $append_cronfile = "echo '";
+        $append_cronfile .= (is_array($cron_jobs)) ? implode("\n", $cron_jobs) : $cron_jobs;
+        $append_cronfile .= "'  >> {$this->cron_file}";
+        $install_cron = "crontab {$this->cron_file}";
+        $this->write_to_file()->exec($append_cronfile, $install_cron)->remove_file();
+
+        return $this;
+    }
+
+    protected function remove_cronjob($cron_jobs=NULL) {
+        if (is_null($cron_jobs)) {
+            $this->error_message("Nothing to remove!  Please specify a cron job or an array of cron jobs.");
+        }
+         
+        $this->write_to_file();
+        $cron_array = file($this->cron_file, FILE_IGNORE_NEW_LINES);
+        if (empty($cron_array)) {
+            $this->error_message("Nothing to remove!  The cronTab is already empty.");
+        }
+         
+        $original_count = count($cron_array);
+        if (is_array($cron_jobs)) {
+            foreach ($cron_jobs as $cron_regex) {
+                $cron_array = preg_grep($cron_regex, $cron_array, PREG_GREP_INVERT);
+            }
+        } else {
+            $cron_array = preg_grep($cron_jobs, $cron_array, PREG_GREP_INVERT);
+        }
+        
+        return ($original_count === count($cron_array)) ? $this->remove_file() : $this->remove_crontab()->append_cronjob($cron_array);
+    }
+
+    protected function remove_crontab() {
+        $this->exec("crontab -r")->remove_file();
+        return $this;
+    }
+
+    /**
+     * @before _secure, _admin
+     */
     public function index() {
-        $this->verify();
+        $this->noview();
+        //$this->verify();
     }
     
     protected function verify() {
@@ -50,17 +162,28 @@ class CRON extends Auth {
     }
 
     protected function saveStats($object, $link, $count) {
-        $stat = new Stat(array(
-            "user_id" => $link->user_id,
-            "link_id" => $link->id,
-            "verifiedClicks" => $count,
-            "shortUrlClicks" => $object->analytics->day->shortUrlClicks,
-            "longUrlClicks" => $object->analytics->day->longUrlClicks,
-            "referrers" => json_encode($object->analytics->day->referrers),
-            "countries" => json_encode($object->analytics->day->countries),
-            "browsers" => json_encode($object->analytics->day->browsers),
-            "platforms" => json_encode($object->analytics->day->platforms)
-        ));
+        $stat = Stat::first(array("link_id = ?" => $link->id));
+        if ($stat) {
+            $stat->verifiedClicks = $count;
+            $stat->shortUrlClicks = $object->analytics->day->shortUrlClicks;
+            $stat->longUrlClicks = $object->analytics->day->longUrlClicks;
+            $stat->referrers = json_encode($object->analytics->day->referrers);
+            $stat->countries = json_encode($object->analytics->day->countries);
+            $stat->browsers = json_encode($object->analytics->day->browsers);
+            $stat->platforms = json_encode($object->analytics->day->platforms);
+        } else {
+            $stat = new Stat(array(
+                "user_id" => $link->user_id,
+                "link_id" => $link->id,
+                "verifiedClicks" => $count,
+                "shortUrlClicks" => $object->analytics->day->shortUrlClicks,
+                "longUrlClicks" => $object->analytics->day->longUrlClicks,
+                "referrers" => json_encode($object->analytics->day->referrers),
+                "countries" => json_encode($object->analytics->day->countries),
+                "browsers" => json_encode($object->analytics->day->browsers),
+                "platforms" => json_encode($object->analytics->day->platforms)
+            ));
+        }
         $stat->save();
         return $stat;
     }
