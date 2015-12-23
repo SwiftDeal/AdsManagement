f0e1d9c6e6f43f80df62aaa865a96540fb288b72
Faizan Ayubi <indianayubi@gmail.com>
2015-12-24 00:07:50 +0530

    Added CouchDB Library

--- application/controllers/publisher.php
+++ application/controllers/publisher.php
@@ -7,6 +7,7 @@
 use Framework\RequestMethods as RequestMethods;
 use Framework\Registry as Registry;
 use ClusterPoint\DB as DB;
+use CouchDB\CouchDB as CouchDB;
 
 class Publisher extends Admin {
 	
@@ -35,5 +36,19 @@ class Publisher extends Admin {
     public function fraud() {
         $this->seo(array("title" => "Fraud Links", "view" => $this->getLayoutView()));
         $view = $this->getActionView();
+    }
+
+    public function nosql() {
+        $this->noview();
+        $couchdb = new CouchDB('stats');
+        $result = $couchdb->get_all_docs();
+
+        // here we get the decoded json from the response
+        $all_docs = $result->getBody(true);
+
+        // then we can iterate through the returned rows and fetch each item using its id.
+        foreach($all_docs->rows as $r => $row) {
+            print_r($couchdb->get_item($row->id));
+        }
     }
 }
