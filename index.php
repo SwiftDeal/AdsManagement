3d496cdea078210d5ed0cb23443cb1443b64dc44
Faizan Ayubi <indianayubi@gmail.com>
2016-01-02 22:27:35 +0530

    Completed Project

--- index.php
+++ index.php
@@ -4,7 +4,7 @@ define("DEBUG", FALSE);
 
 define("APP_PATH", str_replace(DIRECTORY_SEPARATOR, "/", dirname(__FILE__)));
 define("URL", "http://$_SERVER[HTTP_HOST]$_SERVER[REQUEST_URI]");
-define("CDN", "http://chocoghar.com/public/assets/");
+define("CDN", "http://chocoimage.in/");
 
 date_default_timezone_set('Asia/Kolkata');
 ini_set('max_execution_time', 9000);
